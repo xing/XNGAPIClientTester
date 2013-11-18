7b64025be6b622266e0d10a7ba030b68ff6c0ab6
Piet Brauer <piet.brauer@xing.com>
2013-11-18 17:38:48 +0100

    Add example project

---
+++ Example/XNGAPIClientTesterExample/XNGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XNGAppDelegate.h
+//  XNGAPIClientTesterExample
+//
+//  Created by Piet Brauer on 18.11.13.
+//  Copyright (c) 2013 XING AG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XNGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/XNGAPIClientTesterExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  XNGAPIClientTesterExample
+//
+//  Created by Piet Brauer on 18.11.13.
+//  Copyright (c) 2013 XING AG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XNGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XNGAppDelegate class]));
+    }
+}
