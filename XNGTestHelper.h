209e83b905de4c73b1706838a5d28a3690428ff4
Piet Brauer <piet@nerdishbynature.com>
2014-03-24 21:43:07 +0100

    Update year

--- XNGTestHelper.h
+++ XNGTestHelper.h
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2013 XING AG (http://xing.com/)
+// Copyright (c) 2014 XING AG (http://xing.com/)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
